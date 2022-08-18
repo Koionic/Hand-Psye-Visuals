46427dd81b8fd2acc2b803bf3c7ebd5ea16e17db
Koionic <ganonscrap@gmail.com>
2022-08-18 23:25:21 +0800

    finished creating generic system for controller input to midi signal

--- Assets/Scripts/ControllerToMidi.cs
+++ Assets/Scripts/ControllerToMidi.cs
@@ -9,6 +9,24 @@ public class ControllerToMidi : MonoBehaviour
     private InputInterface _inputInterface;
 
     private MidiCommander _midiCommander;
+
+    private float targetLeftX;
+    private float targetLeftY;
+    private float targetRightX;
+    private float targetRightY;
+    
+    private float currentLeftX;
+    private float currentLeftY;
+    private float currentRightX;
+    private float currentRightY;
+
+    private float leftXSpeed;
+    private float leftYSpeed;
+    private float rightXSpeed;
+    private float rightYSpeed;
+    
+    public float lerpSmoothness = 0.2f;
+    public float lerpMaxSpeed = 0.2f;
     
     // Start is called before the first frame update
     void Awake()
@@ -16,35 +34,159 @@ public class ControllerToMidi : MonoBehaviour
         _inputInterface = FindObjectOfType<InputInterface>();
         _midiCommander = FindObjectOfType<MidiCommander>();
     }
+    
+    
 
     private void Start()
     {
+        _inputInterface.face_up.AddListener(FaceUp);
         _inputInterface.face_down.AddListener(FaceDown);
+        _inputInterface.face_left.AddListener(FaceLeft);
+        _inputInterface.face_right.AddListener(FaceRight);
+        
+        _inputInterface.dpad_up.AddListener(DpadUp);
+        _inputInterface.dpad_down.AddListener(DpadDown);
+        _inputInterface.dpad_left.AddListener(DpadLeft);
+        _inputInterface.dpad_right.AddListener(DpadRight);
+        
+        _inputInterface.trigger_left.AddListener(LeftTrigger);
+        _inputInterface.trigger_right.AddListener(RightTrigger);
+        
+        _inputInterface.shoulder_left.AddListener(LeftShoulder);
+        _inputInterface.shoulder_right.AddListener(RightShoulder);
+        
+        _inputInterface.leftStick_x.AddListener(LeftStick_X);
+        _inputInterface.leftStick_y.AddListener(LeftStick_Y);
+        
+        _inputInterface.rightStick_x.AddListener(RightStick_X);
+        _inputInterface.rightStick_y.AddListener(RightStick_Y);
     }
 
+    private void FixedUpdate()
+    {
+        LerpValueThenSendMidi(ref currentLeftX, ref targetLeftX, ref leftXSpeed, ControllerInputButton.Analogue_LeftX);
+        LerpValueThenSendMidi(ref currentLeftY, ref targetLeftY, ref leftYSpeed, ControllerInputButton.Analogue_LeftY);
+        LerpValueThenSendMidi(ref currentRightX, ref targetRightX, ref rightXSpeed, ControllerInputButton.Analogue_RightX);
+        LerpValueThenSendMidi(ref currentRightY, ref targetRightY, ref rightYSpeed, ControllerInputButton.Analogue_RightY);
+    }
+
+    public void LerpValueThenSendMidi(ref float currentValue, ref float targetValue, ref float refTargetValue, ControllerInputButton button)
+    {
+        bool updateMidi = false;
+        
+        if (Mathf.Abs(targetValue - currentValue) > 0.01f)
+        {
+            currentValue = Mathf.SmoothDamp(currentValue, targetValue, ref refTargetValue, lerpSmoothness, lerpMaxSpeed);
+
+            updateMidi = true;
+        }
+        else if (currentValue != targetValue)
+        {
+            currentValue = targetValue;
+            
+            updateMidi = true;
+        }
+
+        if (updateMidi)
+        {
+            SendCCMidi(button, currentValue);
+        }
+    }
+    
+    void SendCCMidi(ControllerInputButton inputButton, float value)
+    {
+        _midiCommander.SendSignal(inputButton, 0, value);
+    }
+    
+    void SendButtonMidi(ControllerInputButton inputButton, InputActionPhase actionPhase)
+    {
+        _midiCommander.SendSignal(inputButton, 0, 100f, actionPhase);
+    }
+
+    public void FaceUp(InputAction.CallbackContext callbackContext)
+    {
+        SendButtonMidi(ControllerInputButton.Face_Up, callbackContext.phase);
+    }
+    
     public void FaceDown(InputAction.CallbackContext callbackContext)
     {
-        if (callbackContext.performed)
-        {
-            SendButtonMidi("FaceDownOn");
-        }
-        else if (callbackContext.canceled)
-        {
-            SendButtonMidi("FaceDownOff");
-        }
+        SendButtonMidi(ControllerInputButton.Face_Down, callbackContext.phase);
+    }
+    
+    public void FaceLeft(InputAction.CallbackContext callbackContext)
+    {
+        SendButtonMidi(ControllerInputButton.Face_Left, callbackContext.phase);
+    }
+    
+    public void FaceRight(InputAction.CallbackContext callbackContext)
+    {
+        SendButtonMidi(ControllerInputButton.Face_Right, callbackContext.phase);
     }
 
-    void SendCCMidi(string midiName, float value)
+    public void DpadUp(InputAction.CallbackContext callbackContext)
     {
-        _midiCommander.SendSignal(midiName, 0, value);
+        SendButtonMidi(ControllerInputButton.Dpad_Up, callbackContext.phase);
     }
     
-    void SendButtonMidi(string midiName)
+    public void DpadDown(InputAction.CallbackContext callbackContext)
     {
-        _midiCommander.SendSignal(midiName, 0, 100f);
+        SendButtonMidi(ControllerInputButton.Dpad_Down, callbackContext.phase);
     }
     
+    public void DpadLeft(InputAction.CallbackContext callbackContext)
+    {
+        SendButtonMidi(ControllerInputButton.Dpad_Left, callbackContext.phase);
+    }
     
+    public void DpadRight(InputAction.CallbackContext callbackContext)
+    {
+        SendButtonMidi(ControllerInputButton.Dpad_Right, callbackContext.phase);
+    }
+    
+    public void LeftStick_X(InputAction.CallbackContext callbackContext)
+    {
+        targetLeftX = (callbackContext.ReadValue<float>() + 1f) / 2f;
+        //SendCCMidi(ControllerInputButton.Analogue_LeftX, callbackContext.phase, callbackContext.ReadValue<float>());
+    }
+    
+    public void LeftStick_Y(InputAction.CallbackContext callbackContext)
+    {
+        targetLeftY = (callbackContext.ReadValue<float>() + 1f) / 2f;
+        //SendCCMidi(ControllerInputButton.Analogue_LeftY, callbackContext.phase, callbackContext.ReadValue<float>());
+    }
+    
+    public void RightStick_X(InputAction.CallbackContext callbackContext)
+    {
+        targetRightX = (callbackContext.ReadValue<float>() + 1f) / 2f;
+        //SendCCMidi(ControllerInputButton.Analogue_RightX, callbackContext.phase, callbackContext.ReadValue<float>());
+    }
+    
+    public void RightStick_Y(InputAction.CallbackContext callbackContext)
+    {
+        targetRightY = (callbackContext.ReadValue<float>() + 1f) / 2f;
+        //SendCCMidi(ControllerInputButton.Analogue_RightY, callbackContext.phase, callbackContext.ReadValue<float>());
+    }
+
+    public void LeftShoulder(InputAction.CallbackContext callbackContext)
+    {
+        SendButtonMidi(ControllerInputButton.Shoulder_Left, callbackContext.phase);
+    }
+    
+    public void RightShoulder(InputAction.CallbackContext callbackContext)
+    {
+        SendButtonMidi(ControllerInputButton.Shoulder_Right, callbackContext.phase);
+    }
+    
+    public void LeftTrigger(InputAction.CallbackContext callbackContext)
+    {
+        SendCCMidi(ControllerInputButton.Trigger_Left, callbackContext.ReadValue<float>());
+    }
+    
+    public void RightTrigger(InputAction.CallbackContext callbackContext)
+    {
+        SendCCMidi(ControllerInputButton.Trigger_Right, callbackContext.ReadValue<float>());
+    }
+
     // Update is called once per frame
     void Update()
     {
--- Assets/Scripts/MidiCommander.cs
+++ Assets/Scripts/MidiCommander.cs
@@ -9,13 +9,39 @@ public enum MidiSignalType
 {
     NoteOn,
     NoteOff,
+    ButtonNote,
     ControlChange
+}
+
+public enum ControllerInputButton
+{
+    Analogue_LeftX,
+    Analogue_LeftY,
+    Analogue_LeftClick,
+    Analogue_RightX,
+    Analogue_RightY,
+    Analogue_RightClick,
+    Shoulder_Left,
+    Shoulder_Right,
+    Trigger_Left,
+    Trigger_Right,
+    Dpad_Up,
+    Dpad_Down,
+    Dpad_Left,
+    Dpad_Right,
+    Face_Up,
+    Face_Down,
+    Face_Left,
+    Face_Right,
+    Select,
+    Start
 }
 
 [Serializable]
 public class MidiCommand
 {
     public string name;
+    public ControllerInputButton inputButton;
     public MidiSignalType signalType;
     public MidiChannel channel;
 
@@ -29,21 +55,34 @@ public class MidiCommand
     /// Velocity when NoteOn/NoteOff, Midi Value when Control Change
     /// </param>
     /// <exception cref="ArgumentOutOfRangeException"></exception>
-    public void ExecuteSignal(int addressNumber, float changeValue)
+    public void ExecuteSignal(InputActionPhase actionPhase, int addressNumber, float changeValue)
     {
         switch (signalType)
         {
+            case MidiSignalType.ButtonNote:
+                switch (actionPhase)
+                {
+                    case InputActionPhase.Performed:
+                        MidiOut.SendNoteOn(channel, addressNumber, changeValue);
+                        Debug.Log(name + " SENDING NOTE ON THROUGH CHANNEL " + channel + " - NOTE " + addressNumber + " - VELOCITY " + changeValue);
+                        break;
+                    case InputActionPhase.Canceled:
+                        MidiOut.SendNoteOff(channel, addressNumber);
+                        Debug.Log(name + " SENDING NOTE OFF THROUGH CHANNEL " + channel + " - NOTE " + addressNumber);
+                        break;
+                }
+                break;
             case MidiSignalType.NoteOn:
                 MidiOut.SendNoteOn(channel, addressNumber, changeValue);
-                Debug.Log(name + " SENDING NOTE ON - NOTE " + addressNumber + " - VELOCITY " + changeValue);
+                Debug.Log(name + " SENDING NOTE ON THROUGH CHANNEL " + channel + " - NOTE " + addressNumber + " - VELOCITY " + changeValue);
                 break;
             case MidiSignalType.NoteOff:
                 MidiOut.SendNoteOff(channel, addressNumber);
-                Debug.Log(name + " SENDING NOTE OFF - NOTE " + addressNumber);
+                Debug.Log(name + " SENDING NOTE OFF THROUGH CHANNEL " + channel + " - NOTE " + addressNumber);
                 break;
             case MidiSignalType.ControlChange:
                 MidiOut.SendControlChange(channel, addressNumber, changeValue);
-                Debug.Log(name + " SENDING CONTROL CHANGE - CONTROLLER " + addressNumber + " - MIDI VALUE " + changeValue);
+                Debug.Log(name + " SENDING CONTROL CHANGE THROUGH CHANNEL " + channel + " - CONTROLLER " + addressNumber + " - MIDI VALUE " + changeValue);
                 break;
             default:
                 throw new ArgumentOutOfRangeException();
@@ -67,13 +106,13 @@ public class MidiCommander : MonoBehaviour
         
     }
 
-    public void SendSignal(string nameToSearch, int addressNumber, float changeValue)
+    public void SendSignal(ControllerInputButton buttonToSearch, int addressNumber, float changeValue, InputActionPhase actionPhase = InputActionPhase.Canceled)
     {
         for (int i = 0; i < _midiCommands.Count; i++)
         {
-            if (_midiCommands[i].name == nameToSearch)
+            if (_midiCommands[i].inputButton == buttonToSearch)
             {
-                _midiCommands[i].ExecuteSignal(addressNumber, changeValue);
+                _midiCommands[i].ExecuteSignal(actionPhase, addressNumber, changeValue);
             }
         }
     }
